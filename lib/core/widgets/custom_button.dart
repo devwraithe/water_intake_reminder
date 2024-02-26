1f046c33f913099dc94e42e576dca735354efddf
ibrahim-ibrahim <folahanmi0001@gmail.com>
2024-02-26 21:31:18 +0100

    allow user to manually log intake

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -2,33 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:water_intake_reminder/core/helpers/text_style_helper.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, this.onPressed});
+
+  final Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.lightBlueAccent,
-          width: 1.26,
-        ),
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.lightBlueAccent.withOpacity(0.4),
-      ),
-      padding: const EdgeInsets.only(
-        top: 17,
-        bottom: 16,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Water (8oz)",
-            style: TextStyleHelper.mdText(
-              weight: FontWeight.w500,
-            ),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.lightBlueAccent,
+            width: 1.26,
           ),
-        ],
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.lightBlueAccent.withOpacity(0.4),
+        ),
+        padding: const EdgeInsets.only(
+          top: 17,
+          bottom: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Water (8oz)",
+              style: TextStyleHelper.mdText(
+                weight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/screens/tabs/home_tab.dart
+++ lib/presentation/screens/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:water_intake_reminder/core/helpers/text_style_helper.dart';
 import 'package:water_intake_reminder/core/widgets/custom_button.dart';
 import 'package:water_intake_reminder/core/widgets/custom_chip.dart';
@@ -79,15 +80,17 @@ class _HomeTabState extends State<HomeTab> {
                 style: TextStyleHelper.lgText(),
               ),
               const SizedBox(height: 26),
-              const Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
+                  const Expanded(
                     child: CustomButton(),
                   ),
                   const SizedBox(width: 16),
                   Expanded(
-                    child: CustomButton(),
+                    child: CustomButton(
+                      onPressed: () => _customIntake(),
+                    ),
                   ),
                 ],
               ),
@@ -95,6 +98,103 @@ class _HomeTabState extends State<HomeTab> {
           ),
         ),
       ),
+    );
+  }
+
+  // Sheet to change metric unit
+  _customIntake() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 28,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(
+                    TablerIcons.circle_x_filled,
+                    color: Colors.lightBlueAccent,
+                    size: 26,
+                  ),
+                  Text(
+                    "Record Your Intake",
+                    style: TextStyleHelper.hlText(
+                      weight: FontWeight.w700,
+                    ),
+                  ),
+                  const Icon(
+                    TablerIcons.circle_check_filled,
+                    color: Colors.lightBlueAccent,
+                    size: 26,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 60),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "1,478",
+                    style: TextStyleHelper.hllText(
+                      weight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    "ml",
+                    style: TextStyleHelper.hllText(
+                      weight: FontWeight.w700,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "(50oz)",
+                style: TextStyleHelper.lgText(
+                  weight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 70),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    child: CustomButton(),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: CustomButton(
+                      onPressed: () => _customIntake(),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    child: CustomButton(),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: CustomButton(
+                      onPressed: () => _customIntake(),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
